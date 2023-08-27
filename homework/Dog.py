468fa3c14f0f16786004642525105dc33a55710b
Kaila_Btw <javacube1@gmail.com>
2023-08-27 07:37:30 -0400

    Make variables private

--- homework/Dog.py
+++ homework/Dog.py
@@ -1,23 +1,23 @@
 # define a dog class
 class Dog:
     def __init__(self, name, age):
-        self.name = name
-        self.age = age
+        self._name = name
+        self._age = age
 
     # getter method
     def get_age(self):
-        return self.age
+        return self._age
 
     def get_name(self):
-        return self.name
+        return self._name
 
     # setter
     def set_age(self, age):
-        self.age = float(age)
+        self._age = float(age)
 
     def set_name(self, name):
-        self.name = name
+        self._name = name
 
     def age_in_dog_years(self):
-        return self.age * 7
+        return self._age * 7
 
